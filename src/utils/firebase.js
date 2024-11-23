a4051fdffa1b15c326c8ec99e3c5fb8ced32be9f
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2024-11-24 00:12:42 +0200

    mm

---
+++ src/utils/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVNE8TFg43v3cZRM4sb-sZV0ubwSpQGJI",
+  authDomain: "svs-app-4f341.firebaseapp.com",
+  projectId: "svs-app-4f341",
+  storageBucket: "svs-app-4f341.firebasestorage.app",
+  messagingSenderId: "105312144565",
+  appId: "1:105312144565:web:c281f4e194019d9ebc02fc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
